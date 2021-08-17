f1b2baa391021d3360212d2306acb0b503ca9ff2
manyakapadia <87424098+manyakapadia@users.noreply.github.com>
2021-08-17 17:00:29 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,34 @@
+class Box{
+    constructor(x,y,width,height){
+        //1) create a new object
+        //properties of the object
+
+        var options={
+            restitution:0.5,
+            friction:1.5,
+            density:1
+        }
+
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+
+        //2) adding to the world
+        World.add(world,this.body);
+    }
+
+    
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        strokeWeight(4);
+        stroke("brown");
+        fill(255);
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+}
